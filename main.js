d52d06c8ba6dfb079a93cb8475d0389f5e411532
graindstone <bannah.chiu@gmail.com>
2024-06-30 16:24:36 -0700

    idk what I'm doing

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-
+let log = [];
+let id = 0;
+const sendmessage = (msg) => {
+  log.unshift({ msg: msg, id: id });
+  $("#messagecontent").html(`<p id="indivlog_${id}" class="indivlog">${msg}</p>`.concat($("#messagecontent").html()));
+  if (log.length > 20) {
+    $(`#indivlog_${log[log.length - 1].id}`).remove();
+    log.pop();
+  }
+}
+$("waffle").click(() => {
+  
+}
