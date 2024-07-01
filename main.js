25d3ca98640ba6e1d8ba82e3ffc47dbffefc1715
graindstone <bannah.chiu@gmail.com>
2024-06-30 19:18:38 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let log = [];
 let id = 0;
 const sendmessage = (msg) => {
   log.unshift({ msg: msg, id: id });
-  document.querySelector("#messagecontent").html(`<p id="indivlog_${id}" class="indivlog">${msg}</p>`.concat(document.querySelector("#messagecontent").html()));
+  document.querySelector("#messagecontent").html(`<p id="indivlog_document.querySelector{id}" class="indivlog">document.querySelector{msg}</p>`.concat(document.querySelector("#messagecontent").html()));
   if (log.length > 20) {
     document.querySelector(`#indivlog_${log[log.length - 1].id}`).remove();
     log.pop();
